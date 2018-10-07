fb8378cb284481171e16b48dcfbf842c012eb817
kunikane <shuheikundesyoo0905@gmail.com>
2018-10-07 13:41:35 +0900

    commit!

--- src/main/java/com/sys5/spui/configuration/WebSecurityConfigration.java
+++ src/main/java/com/sys5/spui/configuration/WebSecurityConfigration.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfigration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
